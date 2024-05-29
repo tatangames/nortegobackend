ced0bf269a5c952d4050aa14ec4f37450a9be18e
tatangames <54828743+tatangames@users.noreply.github.com>
2024-05-29 15:25:56 -0600

    base de registro v2

--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="#" class="brand-link">
         <img src="{{ asset('images/icono-sistema.png') }}" alt="Logo" class="brand-image img-circle elevation-3" >
-        <span class="brand-text font-weight" style="color: white">APP SANTA ANA NORTE</span>
+        <span class="brand-text font-weight" style="color: white">APP SAN</span>
     </a>
 
     <div class="sidebar">
